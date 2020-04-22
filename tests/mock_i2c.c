8ceb45f8d7bd5ae976c9f3ac5dd67da2869f7ebc
Mika Tuupola <tuupola@appelsiini.net>
2020-04-22 18:55:28 +0700

    Add tests for reading and writing the time

--- tests/mock_i2c.c
+++ tests/mock_i2c.c
@@ -3,7 +3,52 @@
 #include "bm8563.h"
 #include "mock_i2c.h"
 
+static uint8_t decimal2bcd (uint8_t decimal)
+{
+    return (((decimal / 10) << 4) | (decimal % 10));
+}
+
+static uint8_t bcd2decimal(uint8_t bcd)
+{
+   return (((bcd >> 4) * 10) + (bcd & 0x0f));
+}
+
 int32_t mock_i2c_read(uint8_t address, uint8_t reg, uint8_t *buffer, uint16_t size) {
+    uint8_t bcd;
+
+    /* 0..59 */
+    bcd = decimal2bcd(30);
+    buffer[0] = bcd & 0b01111111;
+
+    /* 0..59 */
+    bcd = decimal2bcd(15);
+    buffer[1] = bcd & 0b01111111;
+
+    /* 0..23 */
+    bcd = decimal2bcd(23);
+    buffer[2] = bcd & 0b00111111;
+
+    /* 1..31 */
+    bcd = decimal2bcd(27);
+    buffer[3] = bcd & 0b00111111;
+
+    /* 0..6 */
+    bcd = decimal2bcd(1);
+    buffer[4] = bcd & 0b00000111;
+
+    /* 1..12 */
+    bcd = decimal2bcd(11);
+    buffer[5] = bcd & 0b00011111;
+
+    /* If 2000 set the century bit. */
+    if (106 >= 100) {
+        buffer[5] |= BM8563_CENTURY_BIT;
+    }
+
+    /* 0..99 */
+    bcd = decimal2bcd(106 % 100);
+    buffer[6] = bcd & 0b11111111;
+
     return BM8563_ERROR_OK;
 }
 
--- tests/unit.c
+++ tests/unit.c
@@ -6,21 +6,49 @@ TEST should_pass(void) {
     PASS();
 }
 
-TEST i2c_init_should_fail(void) {
+TEST should_fail_init(void) {
     bm8563_t bm;
     uint32_t status;
     bm.read = &mock_failing_i2c_read;
     bm.write = &mock_failing_i2c_write;
 
     ASSERT_FALSE(BM8563_ERROR_OK == bm8563_init(&bm));
+    PASS();
 }
 
-TEST i2c_init_should_pass(void) {
+TEST should_init(void) {
     bm8563_t bm;
     bm.read = &mock_i2c_read;
     bm.write = &mock_i2c_write;
 
     ASSERT(BM8563_ERROR_OK == bm8563_init(&bm));
+    PASS();
+}
+
+TEST should_fail_read_date(void) {
+    struct tm rtc;
+    bm8563_t bm;
+    bm.read = &mock_failing_i2c_read;
+    bm.write = &mock_i2c_write;
+
+    ASSERT(BM8563_ERROR_OK == bm8563_init(&bm));
+    ASSERT_FALSE(BM8563_ERROR_OK == bm8563_read(&bm, &rtc));
+    PASS();
+}
+
+TEST should_read_date(void) {
+    struct tm rtc;
+    char buffer[128];
+    bm8563_t bm;
+    bm.read = &mock_i2c_read;
+    bm.write = &mock_i2c_write;
+
+    ASSERT(BM8563_ERROR_OK == bm8563_init(&bm));
+    ASSERT(BM8563_ERROR_OK == bm8563_read(&bm, &rtc));
+
+    strftime(buffer, 128 ,"%c (day %j)" , &rtc);
+    ASSERT_STR_EQ("Mon Nov 27 23:15:30 2006 (day 331)", &buffer);
+    PASS();
 }
 
 GREATEST_MAIN_DEFS();
@@ -29,8 +57,10 @@ int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();
 
     RUN_TEST(should_pass);
-    RUN_TEST(i2c_init_should_fail);
-    RUN_TEST(i2c_init_should_pass);
+    RUN_TEST(should_fail_init);
+    RUN_TEST(should_init);
+    RUN_TEST(should_fail_read_date);
+    RUN_TEST(should_read_date);
 
     GREATEST_MAIN_END();
 }
