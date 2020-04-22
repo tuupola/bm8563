313913e9a4aebce58309757217d8454d0c77e463
Mika Tuupola <tuupola@appelsiini.net>
2020-04-22 20:49:54 +0700

    Remove unneeded code

--- tests/mock_i2c.c
+++ tests/mock_i2c.c
@@ -39,16 +39,6 @@ SPDX-License-Identifier: MIT
 
 uint8_t memory[BM8563_TIME_SIZE] = {0};
 
-static uint8_t decimal2bcd (uint8_t decimal)
-{
-    return (((decimal / 10) << 4) | (decimal % 10));
-}
-
-static uint8_t bcd2decimal(uint8_t bcd)
-{
-   return (((bcd >> 4) * 10) + (bcd & 0x0f));
-}
-
 int32_t mock_i2c_read(uint8_t address, uint8_t reg, uint8_t *buffer, uint16_t size) {
     memcpy(buffer, memory, size);
     return BM8563_ERROR_OK;
